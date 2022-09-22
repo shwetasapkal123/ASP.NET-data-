0b2b501ce20c1022eaf23562420832b0a142b940
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-09-22 11:55:17 +0530

    [shweta] Binary Search tree code

---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinaryTree
+    {
+        public Node Root { get; set; }
+
+        public bool Add(int value)
+        {
+            Node before = null;
+            Node after = Root;
+
+            while(after != null)
+            {
+                before = after;
+                if (value < after.Data)
+                    after = after.LeftNode;
+                else if(value>after.Data)
+                    after=after.RightNode;
+                else
+                {
+                    return false;
+                }
+            }
+            Node newNode = new Node();
+            newNode.Data = value;
+            if(Root==null)
+            {
+                Root = newNode;
+            }
+            else
+            {
+                if (value < before.Data)
+                    before.LeftNode = newNode;
+                else
+                    before.RightNode = newNode;
+            }
+            return true;
+        }
+
+        public void TravesePreOrder(Node Parent)
+        {
+            if(Parent!=null)
+            {
+                Console.Write("  "+Parent.Data+"");
+                TravesePreOrder(Parent.LeftNode);
+                TravesePreOrder(Parent.RightNode);
+            }
+            
+        }
+        public void TraveseInOrder(Node Parent)
+        {
+            if (Parent != null)
+            {
+                
+                TraveseInOrder(Parent.LeftNode);
+
+                Console.Write("  " + Parent.Data+"  ");
+
+                TraveseInOrder(Parent.RightNode);
+            }
+
+        }
+        public void TravesePostOrder(Node Parent)
+        {
+            if (Parent != null)
+            {
+
+                TravesePostOrder(Parent.LeftNode);
+
+                TravesePostOrder(Parent.RightNode);
+
+                Console.Write("  "+Parent.Data+"  ");
+            }
+
+        }
+
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+            tree.Add(50);
+            tree.Add(70);
+            tree.Add(60);
+            tree.Add(20);
+            tree.Add(90);
+            tree.Add(10);
+            tree.Add(40);
+            tree.Add(100);
+
+            tree.TravesePreOrder(tree.Root);
+            Console.WriteLine();
+            Console.WriteLine();
+            tree.TraveseInOrder(tree.Root);
+            Console.WriteLine();
+            Console.WriteLine();
+            tree.TravesePostOrder(tree.Root);
+
+
+            Dictionary<int, string> result = new Dictionary<int, string>();
+            result.Add(1, "C#");
+            result.Add(2, "C#");
+            result.Add(3, "python");
+            result.Add(5, "C++");
+
+            foreach(KeyValuePair<int, string> pair in result)
+            {
+                Console.WriteLine("  "+pair.Key+"  "+pair.Value);
+                //Console.Write("  "+pair.Value);
+            }
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("one", 1);
+            hashtable.Add(2, "hghjgkj");
+            hashtable.Add(3.45, 56);
+            foreach(DictionaryEntry entry in hashtable)
+            {
+                Console.WriteLine(entry.Key+"  "+entry.Value);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
