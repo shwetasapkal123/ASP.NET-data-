6f07278cfc2cd0879e35a2a724623c307a13f643
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-09-22 12:05:48 +0530

    [shweta] exception handling code

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[3];
+            TestException.TestArgumentException("Shweta");
+            //try
+            //{
+            //    Console.WriteLine("Enter 1st number ");
+            //    int num1 = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Enter 2nd number ");
+            //    int num2 = Convert.ToInt32(Console.ReadLine());
+                
+            //    arr[0] = 1;
+            //    arr[1] = 2;
+            //    arr[2] = 3;
+            //    int res = num1 / num2;
+                
+                
+            //    Console.WriteLine(res);
+            //}
+            
+           
+            //catch (IndexOutOfRangeException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            ////catch(DivideByZeroException e)
+            ////{
+            ////    Console.WriteLine(e.Message);
+            ////}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+            //finally
+            //{
+            //    for (int i = 0; i < 3; i++)
+            //    {
+            //        Console.WriteLine(arr[i]);
+            //    }
+            //}
+           
+            
+
+            
+        }
+    }
+}
---
+++ ExceptionHandling/TestException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHandling
+{
+    public class TestException
+    {
+        public static void TestArgumentException(string name)
+        {
+            try
+            {
+                //"shweta"
+                string res = name.Substring(name.Length+2);
+                Console.WriteLine(res);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
