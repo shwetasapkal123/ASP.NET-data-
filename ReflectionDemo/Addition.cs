ef9694ac3b51d851147dc40e385d4d2aa3b2d75e
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-09-22 12:08:23 +0530

    [shweta] Reflection demo code

---
+++ ReflectionDemo/Addition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReflectionDemo
+{
+    public class Addition
+    {
+        //Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //COnstructors
+        private Addition()
+        {
+
+        }
+        public Addition(int a)
+        {
+
+        }
+        public Addition(int a,string b)
+        {
+
+        }
+        //Methods
+        public void Add()
+        {
+
+        }
+        public void Sub()
+        {
+
+        }
+    }
+}
---
+++ ReflectionDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            //variable metadata
+            //int a = 20;
+            //Type t = a.GetType();
+            //Console.WriteLine(t);
+            //Console.WriteLine(t.Assembly);
+            //Console.WriteLine(t.Name);
+            //Console.WriteLine(t.IsValueType);
+            //Console.WriteLine(t.IsEnum);
+
+            //Constructor metadata
+
+            Type type = typeof(Addition);
+            ConstructorInfo[] constructorInfos = type.GetConstructors(BindingFlags.Public|BindingFlags.Instance);
+
+            foreach(ConstructorInfo constructor in constructorInfos)
+            {
+                Console.WriteLine(constructor);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            MethodInfo[] methodInfos = type.GetMethods(BindingFlags.Public|BindingFlags.Instance);
+
+            foreach(MethodInfo method in methodInfos)
+            {
+                Console.WriteLine(method);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            PropertyInfo[] propertyInfos = type.GetProperties();
+
+            foreach(PropertyInfo property in propertyInfos)
+            {
+                Console.WriteLine(property);
+            }
+
+        }
+    }
+}
