9794e856e495df22665453669417f021d5837972
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-09-22 12:00:41 +0530

    [shweta] RegexDemo code

---
+++ RegexExpEx/PatternForRegex.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexExpEx
+{
+    public class PatternForRegex
+    {
+        public static void ValidFirstName()
+        {
+            Console.WriteLine("Enter your name");
+            string firstName=Console.ReadLine();
+
+            string pattern = "^[A-Za-z]{8}$";
+
+            if(Regex.IsMatch(firstName,pattern))
+            {
+                Console.WriteLine("Vlid first name");
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
