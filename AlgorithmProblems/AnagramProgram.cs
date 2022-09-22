b7b30adf60d543b041cc4ba4e1c8f6199a5b40df
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-09-22 11:57:35 +0530

    [shweta] Insertion sort And Anagram code

---
+++ AlgorithmProblems/AnagramProgram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+    public class AnagramProgram
+    {
+        public static void Anagrametection()
+        {
+            string str1 = "heart";
+            string str2 = "Eartha";
+
+            char[] ch1=str1.ToLower().ToCharArray();
+            char[] ch2=str2.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            String val1 = new String(ch1);
+            String val2 = new String(ch2);
+
+            Console.WriteLine(val1+"  "+val2);
+
+            if(val1==val2)
+                Console.WriteLine("anagram");
+            else
+                Console.WriteLine("Not anagram");
+
+        }
+    }
+}
---
+++ AlgorithmProblems/InsertionSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+    public class InsertionSort
+    {
+        public static void SortArray()
+        {
+            int[] array = new int[] {23,9,11 };
+            int i, j, val, flag;
+
+            for (i=0;i<array.Length;i++)
+            {
+                val = array[i];
+                flag = 0;
+                for(j=i-1;j>=0 && flag!=1;)
+                {
+                    if(val<array[j])
+                    {
+                        array[j + 1] = array[j];
+                        j--;
+                        array[j+1]=val;
+                    }
+                    else 
+                        flag = 1;
+                }
+            }
+
+            foreach(int e in array)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        
+    }
+}
