faffd7e4b20911dd400d59dbd497362492e0d63d
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-09-22 12:10:34 +0530

    [shweta] Annotation demo code

---
+++ AnnotationDemo/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AnnotationDemo
+{
+    public class Employee
+    {
+        [Required(ErrorMessage ="Employee {0} is required")]
+        [StringLength(100,MinimumLength =3,ErrorMessage ="Nmae should be minimum 3 characters and max of 100 characters")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [Range(18,99,ErrorMessage ="Age should be between 18 and 99")]
+        public int Age { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
---
+++ AnnotationDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace AnnotationDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Employee class validation ");
+            Console.WriteLine();
+
+            Employee employee=new Employee();
+            employee.Name = "Sh";
+            employee.Age = 12;
+            employee.PhoneNumber = "1234abc";
+            employee.Email = "abc@g";
+
+            ValidationContext context = new ValidationContext(employee, null, null);
+            List<ValidationResult> results = new List<ValidationResult>();
+            bool valid = Validator.TryValidateObject(employee, context, results, true);
+
+            if(!valid)
+            {
+                foreach(ValidationResult vr in results)
+                {
+                    Console.WriteLine("Member name  {0}",vr.MemberNames.First());
+                    Console.WriteLine("Error message {0} {1}",vr.ErrorMessage,Environment.NewLine);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Name: "+employee.Name+" Age"+employee.Age+" phone number "+employee.PhoneNumber+" Email "+employee.Email); 
+            }
+        }
+    }
+}
