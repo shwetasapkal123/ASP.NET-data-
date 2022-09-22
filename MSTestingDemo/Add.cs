abfc2b3bbc59b1a60424d9b8d4cb59b37b643219
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-09-22 12:03:17 +0530

    [shweta] msTesting code

---
+++ MSTestingDemo/Add.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSTestingDemo
+{
+    public class Add
+    {
+        public int SumOfNumbers(int a, int b )
+        {
+            int sum = 0;
+            if (a == 0)
+                return b;
+            else if (b == 0)
+                return a;
+            else
+            {
+                sum = a + b;
+                return sum;
+            }
+            
+        }
+        
+    }
+}
---
+++ MSTestingDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MSTestingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * AAA principle
+             * arrange= initials inputs
+             * Act= method
+             * Assert= result occured 
+             */
+            Console.WriteLine("Hello World!");
+            Add add = new Add();
+            int res=add.SumOfNumbers(10, 20);
+            Console.WriteLine(res);
+        }
+    }
+}
