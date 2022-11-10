3746acca819b821b62d25209ca07e71695b0f7ee
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-11-10 12:00:49 +0530

    [Shweta] Added FileIO File

---
+++ FileIODemo/FileOperation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileIODemo
+{
+    public class FileOperation
+    {
+        public void ReadFromStreamReader(string path)
+        {
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while((s=sr.ReadLine()) !=null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+
+        public void WriteusingStreamReader(string path)
+        {
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine("I am from bridgelabz");
+                sr.Close();
+
+                Console.WriteLine(File.ReadAllText(path));
+            }
+        }
+
+    }
+}
---
+++ FileIODemo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace FileIODemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            string path = @"D:\Lfp194\FileIODemo\Demo.txt";
+            //CheckFileExists(path);
+            //ReadAllLinesFromFile(path);
+            //ReadAllTextFromFile(path);
+            //FileCopyFromFile(path);
+            //DeleteFromFile();
+            FileOperation fileOperation = new FileOperation();
+            // fileOperation.ReadFromStreamReader(path);
+            fileOperation.WriteusingStreamReader(path);
+        }
+        public static void CheckFileExists(string path)
+        {
+            if(File.Exists(path))
+            {
+                Console.WriteLine("File exists");
+            }
+            else
+                Console.WriteLine("File is not exists");
+        }
+
+        public static void ReadAllLinesFromFile(string path)
+        {
+            string[] lines;
+            lines = File.ReadAllLines(path);
+
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+            Console.WriteLine(lines[2]);
+        }
+        public static void ReadAllTextFromFile(string path)
+        {
+            string line;
+            line=File.ReadAllText(path);
+
+            Console.WriteLine(line);
+        }
+        public static void FileCopyFromFile(string path)
+        {
+            string copyPath = @"D:\Lfp194\FileIODemo\CopyDemo.txt";
+
+            File.Copy(path, copyPath);
+        }
+        public static void DeleteFromFile()
+        {
+            string copyPath = @"D:\Lfp194\FileIODemo\CopyDemo.txt";
+            File.Delete(copyPath);
+        }
+       
+    }
+}
