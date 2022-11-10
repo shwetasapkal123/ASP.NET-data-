ce39d2527672f09bc9008593a1414371ed70abe3
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-11-10 11:41:16 +0530

    [Shweta] Added data types of c# File

---
+++ DataTypesEx/Add.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace DataTypesEx
+{
+    internal class Add
+    {
+        //instance variable
+        public string Name;
+        static string lastName;
+        // method: define code once and use it multiple in the project
+        //class : design/blueprint
+        //object : instance of that class
+
+        //Type of methods
+        //1. access_modifier returnType MethodName()
+        //{
+        //  body/defination of method
+        //}
+        public void Addition()
+        {
+            int num1 = 23;
+            int num2 = 34;
+            int result=num1 + num2;
+            Console.WriteLine("Addition is "+result);
+        }
+        public void Subtraction()
+        {
+            int num1 = 32;
+            int num4 = 34;
+            int result = num4 - num1;
+            Console.WriteLine("Subtraction is " + result);
+        }
+        public int AdditionReturnType()
+        {
+            int num1 = 23;
+            int num2 = 34;
+            int result = num1 + num2;
+            Console.WriteLine(result);
+            return result;
+        }
+        //Paramaterized method
+        public void ParamaterizedMethod(string msg)
+        {
+            Console.WriteLine("Hello "+msg);
+        }
+        public void ParamaterizedIntMethod(int num1,int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine("Addition of paramerized int datatype method is "+result);
+        }
+        //Parameter and return method
+        public float ParameterAndReturnMethod(float number1, float number2)
+        {
+            float result = number1 + number2;
+            return result;
+        }
+        //static method
+        public static void StaticMethod()
+        {
+            Console.WriteLine(" Static Method ");
+        }
+    }   
+}
---
+++ DataTypesEx/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace DataTypesEx
+{
+    internal class Program
+    {
+        //instance variable
+        int num2;
+        static int num3;
+        public const int constVariable=45;
+        readonly string city;
+        static void Main(string[] args)
+        {
+            //Program.constVariable = 56;
+            Program.num3 = 23;
+            int a=Program.num3 = 34;
+            Console.WriteLine(a);
+            Program program = new Program();
+            program.num2 = 23;
+            Program program1 = new Program();
+            program1.num2 = 34;
+
+            //object or instance of class Add
+            Add add=new Add();
+            add.Addition();
+            add.Subtraction();
+            add.ParamaterizedMethod("Shweta Sapkal");
+            add.ParamaterizedIntMethod(23,34);
+            Console.WriteLine();
+            Console.WriteLine("Parameter and return method addition is "+add.ParameterAndReturnMethod(30.33f,45.56f));
+
+            Add.StaticMethod();
+            
+            
+            //Console.WriteLine("Addition using return type is "+ add.AdditionReturnType());
+
+
+            //DataTypes
+            //int a = 34;
+            //Console.WriteLine("integer value "+a);
+            //int b = int.MaxValue;
+            //Console.WriteLine("max value of int datatype " + b);
+            //Console.WriteLine(byte.MaxValue);
+
+            ////float double , decimal 
+            //float f = 2334.45567f;
+            //Console.WriteLine("float value is "+f);
+            //Console.WriteLine(float.MinValue);
+
+            //double d = 225667.78654321890;
+            //Console.WriteLine("Double value is "+d);
+
+            //decimal e = 12345.654321897654m;
+            //Console.WriteLine("decimal value is "+e);
+
+            ////bool  true false 
+            //bool g = true;
+            //int s = 23;
+            //int h = 45;
+            //bool c = s < h;
+            //Console.WriteLine(c);
+
+            ////string  multiple characters in single variable 
+            ////char   single character
+            //string name = "Shweta sapkal khghkgjh";
+            //char ch = 'f';
+            //Console.WriteLine("String values "+name);
+            //Console.WriteLine("Char datatype value"+ch);
+        }
+    }
+}
