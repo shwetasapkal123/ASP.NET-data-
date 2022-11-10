1a8b463f5c977f2bb037965743cef57e8cddc240
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-11-10 11:32:47 +0530

    [Shweta] Added Cab Invoice Generator File

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CabInvoiceGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double Fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine("Total fare is " + Fare);
+        }
+    }
+}
---
+++ CabInvoiceGenerator/RideRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    public class RideRepository
+    {
+        Dictionary<string, List<Ride>> userRides = null;
+
+        public RideRepository()
+        {
+            userRides = new Dictionary<string, List<Ride>>();
+        }
+
+        public void AddRide(string userId, Ride[] rides)
+        {
+            bool ridelist=this.userRides.ContainsKey(userId);
+            try
+            {
+                if(!ridelist)
+                {
+                    List<Ride> list = new List<Ride>();
+                    list.AddRange(rides);
+                    userRides.Add(userId, list);
+                }
+            }
+            catch(CabInvoiceException)
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.NULL_RIDES, "Rides are null");
+            }
+        }
+
+        public Ride[] GetRides(string userId)
+        {
+            try
+            {
+                return this.userRides[userId].ToArray();
+            }
+            catch(CabInvoiceException)
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_USER_ID, "Invalid user Id");
+            }
+        }
+    }
+}
