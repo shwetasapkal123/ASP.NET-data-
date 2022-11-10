62707460199924279b3c94a2379406b6dd693aa3
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-11-10 11:31:00 +0530

    [Shweta] Added Binary Serialization File

---
+++ BinarySerializationDemo/Binary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace BinarySerializationDemo
+{
+    public class Binary
+    {
+        public void Serialize()
+        {
+            Demo demo = new Demo();
+            FileStream file = new FileStream(@"D:\Lfp194\BinarySerializationDemo\Example.txt",FileMode.Create);
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            formatter.Serialize(file, demo);
+        }
+        public void Deserialize()
+        {
+            FileStream fileStream = new FileStream(@"D:\Lfp194\BinarySerializationDemo\Example.txt", FileMode.Open);
+            BinaryFormatter formatter=new BinaryFormatter();
+            Demo desrialize=(Demo)formatter.Deserialize(fileStream);
+
+            Console.WriteLine(desrialize.ApplicatioName+"  "+desrialize.ApplicationID);
+        }
+    }
+
+
+    [Serializable]
+    public class Demo
+    {
+        public string ApplicatioName { get; set; } = "Binary Serialize";
+        public int ApplicationID { get; set; } = 100;
+
+    }
+
+}
---
+++ BinarySerializationDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BinarySerializationDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Binary binary = new Binary();
+            //binary.Serialize();
+            binary.Deserialize();
+        }
+    }
+}
