788b75cfea09de48c61c5f64cc632d94c336b0c8
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-11-10 11:25:00 +0530

    [Shweta] Added Custom Exception File

---
+++ CustomException/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CustomException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Temperature temperature = new Temperature();
+            try
+            {
+                temperature.ShowTemperature();
+            }
+            catch(TemperatureZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ CustomException/TemperatureZeroException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomException
+{
+    public class Temperature
+    {
+        int temperature = 0;
+        public void ShowTemperature()
+        {
+            if(temperature==0)
+            {
+                throw (new TemperatureZeroException("Zero temperature found "));
+            }
+            else
+            {
+                Console.WriteLine("Temperature is "+temperature);
+            }
+        }
+    }
+    public class TemperatureZeroException:Exception
+    {
+        public TemperatureZeroException(string message):base(message)
+        {
+
+        }
+    }
+}
